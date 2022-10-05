aba5c2e87556c8ca5ae065739401112e7c95e018
darklune1929 <59780274+darklune1929@users.noreply.github.com>
2022-10-05 16:14:01 -0500

    Add files via upload

---
+++ 2022-2/Estructura/ColaBanco/main.c
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: cueva.r
+ *
+ * Created on 3 de octubre de 2022, 11:50 AM
+ */
+#include "Colas.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    Cola banco;
+    
+    construir_cola(&banco);
+    
+    return 0;
+}
+
