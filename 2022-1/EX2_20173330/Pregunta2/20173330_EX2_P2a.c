5fceb2f319c803a005148037f1547bd3e375a0d1
darklune1929 <59780274+darklune1929@users.noreply.github.com>
2022-11-30 17:05:36 -0500

    Update 20173330_EX2_P2a.c

--- 2022-1/EX2_20173330/Pregunta2/20173330_EX2_P2a.c
+++ 2022-1/EX2_20173330/Pregunta2/20173330_EX2_P2a.c
@@ -3,49 +3,38 @@
 #include <math.h>
 #define max(x,y) (((x)>(y))? (x) : (y))
 #define min(x,y) (((x)>(y))? (y) : (x))
-int calcular_tiempo(int** tabla,int* tiempos,int N,int R){
-    int sum = 0;
-    for(int i=0;i<=N;i++){
-        tabla[i][0] = 0;
+int calcular_tiempo(int* solucion,int tiempos[5],int N,int R){
+    for(int i=0;i<R;i++){
+        solucion[i] =  tiempos[i];
     }
-    for(int i=1;i<=N;i++){
-        sum += tiempos[i-1];
-        tabla[i][1] = sum;
-    }
-    for(int i=0;i<=R;i++){
-        tabla[0][i] = 0;
-    }
-    for(int i=2;i<=R;i++){
-        int sum_max_tareas=0;
-        for(int j=1;j<=N;j++){
-            sum_max_tareas = max(max(tabla[j][i-1]-tabla[j-1][i],tabla[j-1][i]),tiempos[j-1]);
-            tabla[j][i] = sum_max_tareas;
+    for(int i=R;i<N;i++){
+        int minimo= solucion[i-R];
+        for(int j=i-R;j<i;j++){
+            minimo = min(solucion[j],minimo);
         }
+        solucion[i] = tiempos[i] + minimo;
     }
-    return tabla[N-1][R-1];
+    int mayor = 0;
+    for(int i=N-1;i>=N-R;i--){
+        mayor = max(solucion[i],mayor);
+    }
+    return mayor;
 }
 int main(){
     int N=5;
     int tiempos[] = {15,30,60,45,10};
     int R=2;
-    int** tabla = (int**)malloc((N+1)*sizeof(int*));
-    for(int i=0;i<=N;i++){
-        tabla[i] = (int*)malloc((R+1)*sizeof(int));
-        for(int j=0;j<R;j++){
-        }
-    }
+    int* solucion = (int*)malloc((N)*sizeof(int));
 
-    int tiempo = calcular_tiempo(tabla,tiempos,N,R);
-    printf("El tiempo máximo que utilizarian %d robots es %d min\n",R,tiempo);
+
+    int tiempo = calcular_tiempo(solucion,tiempos,N,R);
+    printf("El tiempo maximo que utilizarian %d robots es %d min\n",R,tiempo);
     for(int i=0;i<N;i++){
         printf("%d ",tiempos[i]);
     }
     printf("\n");
-    for(int i=0;i<N+1;i++){
-        for(int j=0;j<R+1;j++){
-            printf("%02d ",tabla[i][j]);
-        }
-        printf("\n");
+    for(int i=0;i<N;i++){
+            printf("%02d ",solucion[i]);
     }    
 
     return 0;
